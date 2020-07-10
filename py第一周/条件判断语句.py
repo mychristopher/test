ea48a1a4461a87bcbd7a1a44b82d20ef462d2968
cyf2 <1366254420@qq.com>
2020-07-10 19:42:32 +0800

    部分概念添加

---
+++ py第一周/条件判断语句.py
@@ -0,0 +1,7 @@
+#！/usr/bin/python
+# -*- coding: utf-8 -*-
+a = 100
+if a >= 0:
+    print(a)
+else:
+    print(-a)
---
+++ py第一周/输入与输出.py
@@ -0,0 +1,4 @@
+#！/usr/bin/python
+# -*- coding: utf-8 -*-
+name = input('please enter your name: ')
+print('hello,', name)
